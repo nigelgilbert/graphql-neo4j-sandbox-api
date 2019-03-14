ff4e8a895255449fc85b291bea8a661436701574
nigelgilbert <nigelcodes@gmail.com>
2019-03-14 13:36:50 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { GraphQLServer } from 'graphql-yoga'
+import { makeAugmentedSchema } from 'neo4j-graphql-js';
+import { v1 as neo4j } from 'neo4j-driver';
+
+const typeDefs = `
+  type Session {
+      about: String
+      date: Int
+      name: String
+      tags: [Tag] @relation(name: "HAS_TAG", direction: "OUT")
+  }
+  type Tag {
+      name: String
+  }
+`;
+
+const schema = makeAugmentedSchema({ typeDefs });
+
+const driver = neo4j.driver(
+  'bolt://54.172.85.63:32909',
+  neo4j.auth.basic('neo4j', 'meaning-vehicles-jeopardies')
+);
+
+const server = new GraphQLServer({
+  schema,
+  context: { driver }
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
